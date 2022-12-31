aff4e56d99fafa3f9e1df4bad3ace3e0e4d7207c
Yuval Uner <yuvaluner@gmail.com>
2022-12-31 16:19:47 +0200

    Finished album page, added link to album page from song page

---
+++ src/main-app/general-components/artists-list-with-links.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {Link} from "@mui/material";
+import PageEnum from "../page-enum";
+
+class ArtistsListWithLinks extends React.Component<any, any>{
+
+    goToArtistPage = (artistName: string): void => {
+        this.props.setArtistName(artistName);
+        this.props.setPage(PageEnum.ARTIST);
+    }
+
+    createArtistLinks = (): JSX.Element => {
+        let artists: string[] = this.props.artists;
+        let artistLinks: JSX.Element[] = [];
+        for (let i = 0; i < artists.length; i++) {
+            artistLinks.push(<Link
+                href={"#"}
+                onClick={() => this.goToArtistPage(artists[i])}
+                key={artists[i]}>
+                {artists[i]}
+            </Link>);
+            if (i !== artists.length - 1) {
+                artistLinks.push(<span key={artists[i] + "span"}>, </span>);
+            }
+        }
+        return <>{artistLinks}</>;
+    }
+
+    render() {
+        return(
+            <>
+                {this.createArtistLinks()}
+            </>
+        );
+    }
+}
+
+export default ArtistsListWithLinks;
