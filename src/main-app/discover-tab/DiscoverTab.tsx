785a4a4a99c6f5bcc2e32d0ee08f7295b3897811
amiram-yss <amiram-yss@users.noreply.github.com>
2023-01-15 18:55:44 +0200

    Almost done.

--- src/main-app/discover-tab/DiscoverTab.tsx
+++ src/main-app/discover-tab/DiscoverTab.tsx
@@ -13,35 +13,40 @@ class DiscoverTab extends React.Component<any, any>{
     render(){
         return (
             <Drawer
-                className={'discover-menu'}
-            variant={"permanent"}
-            anchor={"left"}
-            sx={{
-                display: { xs: 'none', sm: 'block' },
-                '& .MuiDrawer-paper': { boxSizing: 'border-box', width: `${this.props.width}%` },
-            }}
-            >
-                <h2>Discover</h2>
-                <Toolbar />
-                <Divider />
-                <h5>Recommendations</h5>
-                <p className={"small-text"}>Please note that information in this tab may not be complete - songs with multiple artists
-                    collaborating will only display a single artist in this list</p>
-                <Recommendations
-                    username={this.props.username}
-                    setSongName={this.props.setSongName}
-                    setAlbumName={this.props.setAlbumName}
-                    setArtistName={this.props.setArtistName}
-                    setPage={this.props.setPage}
+                className={'discover-menu drawer'}
+                //style={{backgroundColor: 'red'}}
+                variant={"permanent"}
+                anchor={"left"}
+                sx={{
+                    display: { xs: 'none', sm: 'block' },
+                    '& .MuiDrawer-paper': { boxSizing: 'border-box', width: `${this.props.width}%` },
+                }}
                 >
-                </Recommendations>
-                <Toolbar />
-                <Divider />
-                <h5>Top Artists</h5>
-                <TopArtists
-                    setArtistName={this.props.setArtistName}
-                    setPage={this.props.setPage}
-                ></TopArtists>
+                <div style={{backgroundColor:'#696969',height: '100%', color: '#F5F5F5', display:'block', overflow:'auto'}}>
+                    <h2 className={'discover-menu'} style={{width: '100%'}}>Discover</h2>
+                    <Toolbar />
+                    <Divider />
+                    <h5>Recommendations</h5>
+                    <p className={"small-text"}>Please note that information in this tab may not be complete - songs with multiple artists
+                        collaborating will only display a single artist in this list</p>
+                    <Recommendations
+                        username={this.props.username}
+                        setSongName={this.props.setSongName}
+                        setAlbumName={this.props.setAlbumName}
+                        setArtistName={this.props.setArtistName}
+                        setPage={this.props.setPage}
+                        //style={{backgroundColor: 'red'}}
+                    >
+                    </Recommendations>
+                    <Toolbar />
+                    <Divider/>
+                    <h5>Top Artists</h5>
+                    <TopArtists
+                        setArtistName={this.props.setArtistName}
+                        setPage={this.props.setPage}
+                        className={'drawer'}
+                    ></TopArtists>
+                </div>
             </Drawer>
         );
     }
